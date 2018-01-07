c3f30d8c7d368e36c9cdfbb7a3b8138996ade157
Antonio Sv <jesus.antoniosv@gmail.com>
2018-01-07 15:40:02 +0200

    Adapting structure to fit reference implementation of Hila5

--- core/src/main/java/org/bouncycastle/pqc/crypto/hila5/Hila5.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/hila5/Hila5.java
@@ -10,12 +10,67 @@ import org.bouncycastle.crypto.digests.SHA3Digest;
 
 class Hila5
 {
-    private static final boolean STATISTICAL_TEST = false;
-    public static final int AGREEMENT_SIZE = 32;
-    public static final int POLY_SIZE = Params.N;
+    static final int HILA5_N = 1024;
+    static final int HILA5_Q = 12289;
+    //    static final int K = 16; /* used in sampler */
+    static final int HILA5_B = 799;
+
+    static final int HILA5_MAX_ITER = 100;
+    static final int HILA5_SEED_LEN = 32;
+    static final int HILA5_KEY_LEN = 32;
+    static final int HILA5_ECC_LEN = 30;
+    static final int HILA5_PACKED1 = (HILA5_N / 8);
+    static final int HILA5_PACKED14 = (14 * HILA5_N / 8);
+    static final int HILA5_PAYLOAD_LEN = (HILA5_KEY_LEN + HILA5_ECC_LEN);
+    static final int HILA5_PUBKEY_LEN = (HILA5_SEED_LEN + HILA5_PACKED14);
+    static final int HILA5_PRIVKEY_LEN = (HILA5_PACKED14 + 32);
+    static final int HILA5_CIPHERTEXT_LEN = (HILA5_PACKED14 + HILA5_PACKED1 + HILA5_PAYLOAD_LEN + HILA5_ECC_LEN);
+
+
+    // temporal   
     public static final int SENDA_BYTES = Params.POLY_BYTES + Params.SEED_BYTES;
     public static final int SENDB_BYTES = Params.POLY_BYTES + Params.REC_BYTES;
+    public static final int POLY_SIZE = Params.N;
 
+    // to get random bytes:
+    // byte[] seed = new byte[HILA5_SEED_LEN];
+    // SecureRandom rand; rand.nextBytes(seed);
+
+
+    // == Rings and Number Theoratic Transforms ==================================
+
+    static int[] pow1945 = new int[2048];  //powers of g=1945 mod q
+    static boolean pow1945_ok = false;  //true after initialization
+
+    public static void init_pow1945()
+    {
+	if (pow1945_ok)                     // nothing to do then
+	    return;
+
+	int x = 1;                          // 1945^0 = 1
+	for (int i = 0; i < 2048; i++) {    // 1945^1024 = -1 (mod q)
+	    pow1945[i] = x;
+	    x = (1945 * x) % HILA5_Q;       // consecutive powers
+	}
+	pow1945_ok = true;                    // table now ok
+    }
+
+    // Scalar multiplication: v = c * v
+    // Is v passed by reference? i.e. is it modified?
+    public static void smul(int[] v, int c)
+    {
+	for (int i = 0; i < HILA5_N; i++)
+	    v[i] = (c * v[i]) % HILA5_Q;
+    }
+
+    // Pointwise multiplication: d = a (*) b.
+    public static void vmul(int[] d, int[] a, int[] b)
+    {
+	for (int i = 0; i < HILA5_N; i++)
+	    d[i] = (a[i] * b[i]) % HILA5_Q;
+    }
+
+    
     public static void keygen(SecureRandom rand, byte[] send, short[] sk)
     {
 	return;
--- core/src/main/java/org/bouncycastle/pqc/crypto/hila5/Params.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/hila5/Params.java
@@ -4,9 +4,23 @@ class Params
 {
     static final int N = 1024;
     static final int K = 16; /* used in sampler */
-    static final int Q = 12289; 
+    static final int Q = 12289;
+    static final int B = 799;  
 
     static final int POLY_BYTES = 1792;
     static final int REC_BYTES = 256;
     static final int SEED_BYTES = 32;     // care changing this one - connected to digest size used.
+   
+/* missing params
+#define HILA5_MAX_ITER          100
+#define HILA5_KEY_LEN           32
+#define HILA5_ECC_LEN           30
+#define HILA5_PACKED1           (HILA5_N / 8)
+#define HILA5_PACKED14          (14 * HILA5_N / 8)
+#define HILA5_PAYLOAD_LEN       (HILA5_KEY_LEN + HILA5_ECC_LEN)
+#define HILA5_PUBKEY_LEN        (HILA5_SEED_LEN + HILA5_PACKED14)
+#define HILA5_PRIVKEY_LEN       (HILA5_PACKED14 + 32)
+#define HILA5_CIPHERTEXT_LEN    (HILA5_PACKED14 + HILA5_PACKED1 + \
+                                HILA5_PAYLOAD_LEN + HILA5_ECC_LEN)
+*/    
 }
